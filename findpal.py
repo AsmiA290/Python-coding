4e1a5c7c4646071adac74088fbfa79c3155caf0b
AsmiA290 <86789001+AsmiA290@users.noreply.github.com>
2021-07-08 09:30:24 -0500

    Add files via upload
    This quick and easy to read code takes any input and tell you if it's a palindrome.
     INPUT>>>TaCo cAt 
    Process: 1. taco cat 2. tacocat
    OUTPUT>>> TRUE

---
+++ findpal.py
@@ -0,0 +1,30 @@
+''' 
+input=str
+output= bolean 
+'''
+
+def checkPalindrome(input):
+    # first, change the value of input to the stripped value
+    
+    eout = [e for e in input if e.isalnum()] # this list comprehension is the same as lines 10-13
+    # eout = []
+    # for e in input:
+    #     if e.isalnum():
+    #         eout.append(e)
+
+    input = ''.join(eout).lower() # join the letters and ignore case
+
+    # save the  input, then check if it is equal to the flipped version
+    return input == input[::-1]
+    #input.strip()
+
+def main():
+     input="tacocat"
+     #checkPalindrome(input)
+     answer=checkPalindrome(input)
+     print (answer)
+if __name__ == "__main__":
+    main()
+
+
+
